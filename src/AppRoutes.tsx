4c1f2ecb9d425452e6546fb453817325d4991d05
Devam Barbahya <barbhaya.devam@gmail.com>
2024-03-07 18:23:13 +0530

    responsive landing page

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,14 +1,22 @@
-import { Navigate, Route, Routes } from "react-router-dom"
-import Layout from "./layouts/layout"
+import { Navigate, Route, Routes } from "react-router-dom";
+import Layout from "./layouts/layout";
+import HomePage from "./pages/HomePage";
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<Layout>HOME PAGE</Layout>}/>
-      <Route path="/user-profile" element={<span>USER PROFILE PAGE</span>}/>
-      <Route path="*" element={<Navigate to="/" />}/>
+      <Route
+        path="/"
+        element={
+          <Layout>
+            <HomePage />
+          </Layout>
+        }
+      />
+      <Route path="/user-profile" element={<span>USER PROFILE PAGE</span>} />
+      <Route path="*" element={<Navigate to="/" />} />
     </Routes>
-  )
-}
+  );
+};
 
-export default AppRoutes
+export default AppRoutes;
