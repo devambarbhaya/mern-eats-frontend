344942552e9eb4672d38d0a1b4429e9de3c6cbc7
Devam Barbahya <barbhaya.devam@gmail.com>
2024-03-10 12:19:37 +0530

    user profile page

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -2,6 +2,7 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./layouts/layout";
 import HomePage from "./pages/HomePage";
 import AuthCallbackPage from "./pages/AuthCallbackPage";
+import UserProfilePage from "./pages/UserProfilePage";
 
 const AppRoutes = () => {
   return (
@@ -9,13 +10,20 @@ const AppRoutes = () => {
       <Route
         path="/"
         element={
-          <Layout>
+          <Layout showHero>
             <HomePage />
           </Layout>
         }
       />
       <Route path="/auth-callback" element={<AuthCallbackPage />} />
-      <Route path="/user-profile" element={<span>USER PROFILE PAGE</span>} />
+      <Route
+        path="/user-profile"
+        element={
+          <Layout>
+            <UserProfilePage />
+          </Layout>
+        }
+      />
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
---
+++ src/pages/UserProfilePage.tsx
@@ -0,0 +1,12 @@
+import UserProfileForm from "@/forms/user-profile-form/UserProfileForm";
+
+const UserProfilePage = () => {
+  return (
+    <UserProfileForm 
+      onSave={() => {}}
+      isLoading={true}
+    />
+  );
+}
+
+export default UserProfilePage;
