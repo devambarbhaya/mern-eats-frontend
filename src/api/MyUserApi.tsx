9c391a1a2e674c122b6ad82e244477ebbdf489bd
Devam Barbahya <barbhaya.devam@gmail.com>
2024-03-10 19:56:02 +0530

    toast notification

--- src/api/MyUserApi.tsx
+++ src/api/MyUserApi.tsx
@@ -1,5 +1,6 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import { useMutation } from "react-query";
+import { toast } from "sonner";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
@@ -73,10 +74,18 @@ export const useUpdateMyUser = () => {
     mutateAsync: updateUser,
     isLoading,
     isSuccess,
-    isError,
     error,
     reset,
   } = useMutation(updateMyUserRequest);
+
+  if(isSuccess) {
+    toast.success("User updated successfully")
+  }
+
+  if(error) {
+    toast.error(error.toString());
+    reset()
+  }
 
   return {
     updateUser,
