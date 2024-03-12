8b9ac033209948b39dc3e85edc744faaba88c533
Devam Barbahya <barbhaya.devam@gmail.com>
2024-03-12 18:28:08 +0530

    create restaurant form

---
+++ src/pages/ManageRestaurantPage.tsx
@@ -0,0 +1,9 @@
+import ManageRestaurantForm from "@/forms/user-profile-form/manage-restaurant-form/ManageRestaurantForm";
+
+const ManageRestaurantPage = () => {
+  return (
+    <ManageRestaurantForm />
+  );
+}
+ 
+export default ManageRestaurantPage;
