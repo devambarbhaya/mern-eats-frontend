8d8186bc3b46b22af8af331055e27c16b0fdf67a
Devam Barbahya <barbhaya.devam@gmail.com>
2024-03-14 12:13:37 +0530

    search page layout

---
+++ src/components/SearchResultInfo.tsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom";
+
+type Props = {
+  total: number;
+  city: string;
+};
+
+const SearchResultInfo = ({ total, city }: Props) => {
+  return (
+    <div className="text-xl font-bold flex flex-col gap-3 justify-between lg:items-center lg:flex-row">
+      <span>
+        {total} Restaurants found in {city}
+        <Link
+          to={"/"}
+          className="text-sm font-semibold underline cursor-pointer text-blue-500 ml-1"
+        >
+          Change location
+        </Link>
+      </span>
+      Insert sort dropdown here
+    </div>
+  );
+};
+
+export default SearchResultInfo;
--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,20 +1,31 @@
 import { useSearchRestaurants } from "@/api/RestaurantApi";
+import SearchResultCard from "@/components/SearchResultCard";
+import SearchResultInfo from "@/components/SearchResultInfo";
 import { useParams } from "react-router-dom";
 
 const SearchPage = () => {
-  const { city } =  useParams();
-  const { results } = useSearchRestaurants(city)
+  const { city } = useParams();
+  const { results, isLoading } = useSearchRestaurants(city);
+
+  if (isLoading) {
+    <span>Loading...</span>;
+  }
+
+  if (!results?.data || !city) {
+    return <span>No results found.</span>;
+  }
 
   return (
-    <span>
-      User searched for {city}<br />
-      <span>{results?.data.map((restaurant) => (
-        <span>
-          Found - {restaurant.restaurantName}, {restaurant.city}
-        </span>
-      ))}</span>
-    </span>
+    <div className="grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5">
+      <div id="cuisines-list">Insert Cuisines here</div>
+      <div id="main-content" className="flex flex-col gap-5">
+        <SearchResultInfo total={results?.pagination?.total} city={city} />
+        {results?.data.map((restaurant) => (
+          <SearchResultCard restaurant={restaurant} />
+        ))}
+      </div>
+    </div>
   );
-}
- 
+};
+
 export default SearchPage;
