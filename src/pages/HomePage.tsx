b0a9f4edce5a6efcbac1dbf5615580403ec78645
Devam Barbahya <barbhaya.devam@gmail.com>
2024-03-14 09:55:11 +0530

    searchbar component

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,14 +1,26 @@
 import landingImage from "../assets/landing.png"
 import appDownloadImage from "../assets/appDownload.png"
+import SearchBar, { SearchForm } from "@/components/SearchBar";
+import { useNavigate } from "react-router-dom";
+
 
 const HomePage = () => {
+  const navigate = useNavigate();
+
+  const handleSearchSubmit = (searchFormValues: SearchForm) => {
+    navigate({
+      pathname: `/search/${searchFormValues.searchQuery}`
+    })
+  }
+
   return (
     <div className="flex flex-col gap-12">
-      <div className="bg-white rounded-lg shadow-mdp py-8 flex flex-col gap-5 text-center -mt-16">
+      <div className="bg-white rounded-lg shadow-mdp py-8 flex flex-col gap-5 text-center -mt-16 md:px-32">
         <h1 className="text-5xl font-bold tracking-tight text-orange-600">
           Tuck into a takeaway today!
         </h1>
         <span className="text-xl">Food is just a click away!</span>
+        <SearchBar placeholder="Search by City" onSubmit={handleSearchSubmit}/>
       </div>
       <div className="grid md:grid-cols-2 gap-5">
         <img src={landingImage}/>
