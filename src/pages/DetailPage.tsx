3aca2d66c3fe8001068465f30a6af8f3152c2175
Devam Barbahya <barbhaya.devam@gmail.com>
2024-03-18 04:07:26 +0530

    add to cart

--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -1,12 +1,53 @@
 import { useGetRestaurant } from "@/api/RestaurantApi";
 import MenuItemCard from "@/components/MenuItem";
+import OrderSummary from "@/components/OrderSummary";
 import RestaurantInfo from "@/components/RestaurantInfo";
 import { AspectRatio } from "@/components/ui/aspect-ratio";
+import { Card } from "@/components/ui/card";
+import { MenuItem as MenuItemType } from "@/types";
+import { useState } from "react";
 import { useParams } from "react-router-dom";
+
+export type CartItem = {
+  _id: string;
+  name: string;
+  price: number;
+  quantity: number;
+};
 
 const DetailPage = () => {
   const { restaurantId } = useParams();
   const { restaurant, isLoading } = useGetRestaurant(restaurantId);
+  const [cartItems, setCartItems] = useState<CartItem[]>([]);
+  
+  const addToCart = (menuItem: MenuItemType) => {
+    setCartItems((prevCartItems) => {
+      const existingCartItem = prevCartItems.find(
+        (cartItem) => cartItem._id === menuItem._id
+      );
+      let updatedCartItems;
+
+      if (existingCartItem) {
+        updatedCartItems = prevCartItems.map((cartItem) =>
+          cartItem._id === menuItem._id
+            ? { ...cartItem, quantity: cartItem.quantity + 1 }
+            : cartItem
+        );
+      } else {
+        updatedCartItems = [
+          ...prevCartItems,
+          {
+            _id: menuItem._id,
+            name: menuItem.name,
+            price: menuItem.price,
+            quantity: 1,
+          },
+        ];
+      }
+
+      return updatedCartItems;
+    });
+  };
 
   if (isLoading || !restaurant) {
     return <div>Loading...</div>;
@@ -22,11 +63,16 @@ const DetailPage = () => {
       </AspectRatio>
       <div className="grid md:grid-cols-[4fr_2fr] gap-5 md:px-32">
         <div className="flex flex-col gap-4">
-          <RestaurantInfo restaurant={restaurant}/>
-          <span className="text-2xl font-bold tracking-tight">Menu</span>
+          <RestaurantInfo restaurant={restaurant} />
+          <span className="text-2xl font-bold tracking-tight">Menu <span className="text-xs">(Click to add to cart)</span></span>
           {restaurant.menuItems.map((menuItem) => (
-            <MenuItemCard menuItem={menuItem}/>
+            <MenuItemCard menuItem={menuItem} addToCart={() => addToCart(menuItem)} />
           ))}
+        </div>
+        <div className="">
+          <Card>
+            <OrderSummary restaurant={restaurant} cartItems={cartItems} />
+          </Card>
         </div>
       </div>
     </div>
